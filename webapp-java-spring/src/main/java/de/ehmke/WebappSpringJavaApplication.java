9aef8d24d089d941ee7c986e1a618141b5b4624f
Christian Ehmke <christian@ehmke.email>
2015-12-06 11:57:48 +0100

    init setup.

---
+++ webapp-java-spring/src/main/java/de/ehmke/WebappSpringJavaApplication.java
@@ -0,0 +1,12 @@
+package de.ehmke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappSpringJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappSpringJavaApplication.class, args);
+    }
+}
---
+++ webapp-php-symfony/README.md
@@ -0,0 +1,4 @@
+webapp-php-symfony
+==================
+
+A Symfony project created on December 6, 2015, 11:01 am.
